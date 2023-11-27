7932d44b26f4dd991a0a84cc0696487a818a0e2e
Ruslan Khismatullin <Ruslanwar200-10@mail.ru>
2023-11-27 16:57:34 +1100

    задача3

--- Massiv_DZ3/Program.cs
+++ Massiv_DZ3/Program.cs
@@ -48,45 +48,97 @@
 // Задача 2: Задайте массив на 10 целых чисел. Напишите программу, 
 // которая определяет количество чётных чисел в массиве.
 
-int[] FillArray(int size)
+// int[] FillArray(int size)
+// {
+//     int[] array = new int[size];
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(1, 1001);
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[] InMass)
+// {
+//     Console.Write("Массив с элементами: ");
+//     for(int i = 0; i < InMass.Length; i++)
+//     {
+//         Console.Write(InMass[i] + " ");
+//     }
+// }
+
+// void fevenNumbers(int[] num) 
+// {
+//     int[] n = new int[num.Length];
+//     Console.WriteLine(); 
+//     Console.Write("Элементы массива, значения которых четное: "); 
+//     int countWithoutZeros = 0;
+
+//     for (int i = 0; i < num.Length; i++) {
+//         if (num[i] % 2 == 0) {
+//             Console.Write(num[i] + " ");
+//             n[countWithoutZeros] = num[i];
+//             countWithoutZeros++;
+//         }
+//     }
+
+//     Console.WriteLine();
+//     Console.WriteLine($"Количество чётных чисел в массиве: {countWithoutZeros}");
+// }
+
+// int sizeMass = 10;
+// int[] mass = FillArray(sizeMass);
+// PrintArray(mass);
+// fevenNumbers(mass);
+
+// Задача 3: Задайте массив из вещественных чисел с ненулевой дробной частью. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+static double[] AFAWNZD(int length) // AFAWNZD = AutoFillArrayWithNonZeroDecimals
 {
-    int[] array = new int[size];
-    for(int i = 0; i < array.Length; i++)
+    double[] array = new double[length];
+    Random random = new Random();
+
+    for (int i = 0; i < length; i++)
     {
-        array[i] = new Random().Next(1, 1001);
+        double randomNumber = Math.Round(random.NextDouble() * 10, 2);
+        array[i] = randomNumber;
     }
     return array;
 }
 
-void PrintArray(int[] InMass)
+void PrintArray(double[] InMass)
 {
     Console.Write("Массив с элементами: ");
-    for(int i = 0; i < InMass.Length; i++)
+    for (int i = 0; i < InMass.Length; i++)
     {
         Console.Write(InMass[i] + " ");
     }
 }
 
-void fevenNumbers(int[] num) 
+void DON(double[] InMass) // DON = DifferenceOofNnumbers
 {
-    int[] n = new int[num.Length];
-    Console.WriteLine(); 
-    Console.Write("Элементы массива, значения которых четное: "); 
-    int countWithoutZeros = 0;
+    double max = InMass[0];
+    double min = InMass[0];
 
-    for (int i = 0; i < num.Length; i++) {
-        if (num[i] % 2 == 0) {
-            Console.Write(num[i] + " ");
-            n[countWithoutZeros] = num[i];
-            countWithoutZeros++;
-        }
+    foreach (double num in InMass)
+    {
+        if (num > max)
+            max = num;
+
+        if (num < min)
+            min = num;
     }
 
+    double difference = max - min;
+
     Console.WriteLine();
-    Console.WriteLine($"Количество чётных чисел в массиве: {countWithoutZeros}");
+    Console.WriteLine($"Максимальный элемент: {max}");
+    Console.WriteLine($"Минимальный элемент: {min}");
+    Console.WriteLine($"Разница: {difference}");
 }
 
-int sizeMass = 10;
-int[] mass = FillArray(sizeMass);
-PrintArray(mass);
-fevenNumbers(mass);
+int arrayLength = 25;
+double[] myArray = AFAWNZD(arrayLength);
+PrintArray(myArray);
+DON(myArray);
