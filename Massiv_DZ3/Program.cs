8293c4dc72ff03faede2a51f37eabe6f36488f91
Ruslan Khismatullin <Ruslanwar200-10@mail.ru>
2023-11-27 16:53:28 +1100

    задача2

--- Massiv_DZ3/Program.cs
+++ Massiv_DZ3/Program.cs
@@ -1,45 +1,92 @@
 ﻿// Задача 1: Задайте одномерный массив из 10 целых чисел от 1 до 100.
 // Найдите количество элементов массива, значения которых лежат в отрезке [20,90]
 
+// int[] FillArray(int size)
+// {
+// int[] array = new int[size];
+// for(int i = 0; i < array.Length; i++)
+// {
+// array[i] = new Random().Next(1, 101);
+// }
+// return array;
+// }
+
+// void PrintArray(int[] InMass)
+// {
+// Console.Write("Массив с элементами: ");
+// for(int i = 0; i < InMass.Length; i++)
+// {
+// Console.Write(InMass[i] + " ");
+// }
+// }
+
+// void Filter(int[] num)
+// {
+// int[] n = new int[num.Length];
+// Console.WriteLine();
+// Console.Write("Элементы массива, значения которых лежат в отрезке [20, 90]: ");
+
+// for (int i = 0; i < num.Length; i++)
+// {
+// if (num[i] >= 20 && num[i] <= 90)
+// {
+// Console.Write(num[i] + " ");
+// n[i] = num[i];
+// }
+// }
+// int countWithoutZeros = n.Count(x => x != 0);
+// Console.WriteLine();
+// Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [20,90]: {countWithoutZeros}");
+// }
+
+// int sizeMass = 10;
+// int[] mass = FillArray(sizeMass);
+// PrintArray(mass);
+// Filter(mass);
+
+
+// Задача 2: Задайте массив на 10 целых чисел. Напишите программу, 
+// которая определяет количество чётных чисел в массиве.
+
 int[] FillArray(int size)
 {
-int[] array = new int[size];
-for(int i = 0; i < array.Length; i++)
-{
-array[i] = new Random().Next(1, 101);
-}
-return array;
+    int[] array = new int[size];
+    for(int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 1001);
+    }
+    return array;
 }
 
 void PrintArray(int[] InMass)
 {
-Console.Write("Массив с элементами: ");
-for(int i = 0; i < InMass.Length; i++)
-{
-Console.Write(InMass[i] + " ");
-}
+    Console.Write("Массив с элементами: ");
+    for(int i = 0; i < InMass.Length; i++)
+    {
+        Console.Write(InMass[i] + " ");
+    }
 }
 
-void Filter(int[] num)
+void fevenNumbers(int[] num) 
 {
-int[] n = new int[num.Length];
-Console.WriteLine();
-Console.Write("Элементы массива, значения которых лежат в отрезке [20, 90]: ");
+    int[] n = new int[num.Length];
+    Console.WriteLine(); 
+    Console.Write("Элементы массива, значения которых четное: "); 
+    int countWithoutZeros = 0;
 
-for (int i = 0; i < num.Length; i++)
-{
-if (num[i] >= 20 && num[i] <= 90)
-{
-Console.Write(num[i] + " ");
-n[i] = num[i];
-}
-}
-int countWithoutZeros = n.Count(x => x != 0);
-Console.WriteLine();
-Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [20,90]: {countWithoutZeros}");
+    for (int i = 0; i < num.Length; i++) {
+        if (num[i] % 2 == 0) {
+            Console.Write(num[i] + " ");
+            n[countWithoutZeros] = num[i];
+            countWithoutZeros++;
+        }
+    }
+
+    Console.WriteLine();
+    Console.WriteLine($"Количество чётных чисел в массиве: {countWithoutZeros}");
 }
 
 int sizeMass = 10;
 int[] mass = FillArray(sizeMass);
 PrintArray(mass);
-Filter(mass);
+fevenNumbers(mass);
